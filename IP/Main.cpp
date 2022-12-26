0c288ffc8763afcb24b96fb3280ccf61e51f8ffb
Velmeri <danielmikhh2gmail.com>
2022-12-26 16:32:02 +0100

    123

--- IP/Main.cpp
+++ IP/Main.cpp
@@ -19,145 +19,30 @@ int GetComand(string comand) {
 	return InvalidСommand;
 }
 
-int main()
-{
-	struct product {
-		string name;
-		double price;
+#pragma once
 
-		product(string name, double price) {
-			this->name = name;
-			this->price = price;
-		}
-	};
+#include "resource1.h"
+#include <windows.H>
+#include <windowsX.h>
+#include <commctrl.h>
 
-	const short SIZE = 1;
-	const string ComandList[SIZE] = { "GetPrice" };
+#pragma comment(lib,"comctl32")
+#pragma comment(lib, "winmm.lib")
 
-	product Laptop("laptop", 1200);
-	product Phone("phone", 500);
-	product Tablet("tablet", 700);
+HINSTANCE hInst;
 
-	const short PRODUCTCOUNT = 3;
-	product ProductList[PRODUCTCOUNT] = { Laptop, Phone, Tablet };
-
-	int nChoice;
-	int port = 24242;  //âûáèðàåì ïîðò
-	string ipAddress = "127.0.0.1"; //Àäðåñ ñåðâåðà
-	//string ipAddress = "10.4.3.8";
-	char receiveMessage[MAXSTRLEN];
-	char sendMessage[MAXSTRLEN];
-
-	cout << "1) Start server" << endl;
-	cout << "2) Connect to server" << endl;
-	cout << "3) Exit" << endl;
-	cin >> nChoice;
-	switch (nChoice)
-	{
-	case 1:
-	{
-		ServerSocket server;
-		cout << "Starting server..." << endl;
-		//Çàïóñêàåì ñåðâåð
-		server.StartHosting(port);
-		while (true)
-		{
-			cout << "\tWaiting..." << endl;
-			//Ïîëó÷àåì äàííûå îò êëèåíòà
-			//è ñîõðàíÿåì â ïåðåìåííîé receiveMessage
-			server.ReceiveData(receiveMessage, MAXSTRLEN);
-			cout << "Received: " << receiveMessage << endl;
-			switch (GetComand(receiveMessage))
-			{
-			case GetPrice:
-			{
-				bool is_skip = 0;
-				int price = 0;
-				server.SendData("\nServer: please enter name of the product");
-				server.ReceiveData(receiveMessage, MAXSTRLEN);
-				for (unsigned short i = 0; receiveMessage[i] != '\0'; i++)
-					if (receiveMessage[i] >= 'A' && receiveMessage[i] <= 'Z') receiveMessage[i] += 'a' - 'A';
-				for (unsigned short i = 0; i < PRODUCTCOUNT; i++)
-					if (ProductList[i].name == receiveMessage) price = ProductList[i].price;
-				if (price == 0)
-				{
-					server.SendData("\nServer: product not found");
-					break;
-				}
-				else {
-					server.SendData("\nServer: please enter count");
-					server.ReceiveData(receiveMessage, MAXSTRLEN);
-					int res = 0;
-					for (int i = 0; i < strlen(receiveMessage); i++) {
-						if (receiveMessage[i] >= '0' && receiveMessage[i] <= '9' && i > 0 || receiveMessage[i] >= '1' && receiveMessage[i] <= '9' && i == 0)
-							res = res * 10 + receiveMessage[i] - '0';
-						else {
-							server.SendData("\nServer: you entered unacceptable value");
-							is_skip = 1;
-						}
-					}
-					if (is_skip) break;
-					string a = "\nServer: the price of the product is " + to_string(res * price) + "$";
-					server.SendData(a);
-				}
-			}
-			break;
-
-			case HELP:
-			{
-				server.SendData("\nServer:\n\tGetPrice to find out the price of a product,\n\thelp to list all commands,\n\tend to finish work.");
-			}
-			break;
-
-			case END:
-			{
-				server.SendData("end");
-				return 0;
-			}
-
-			default:
-				server.SendData("\nServer: you entered a non-existent command");
-				break;
-			}
-		}
-	}
-	break;
-	case 2:
-	{
-		//cout << "Enter an IP address: " << endl;
-				//Çàïðàøèâàåì IP ñåðâåðà
-				//cin >> ipAddress;
-		ClientSocket client;
-		//ïîäêëþ÷àåìñÿ ê ñåðâåðó
-		client.ConnectToServer(ipAddress.c_str(), port);
-		while (true)
-		{
-			cout << "\nClient: ";
-			//îòïðàâëÿåì ñîîáùåíèå
-			client.SendDataMessage();
-			//ïîëó÷àåì îòâåò
-			client.ReceiveData(receiveMessage, MAXSTRLEN);
-			cout << receiveMessage << endl;
-			if (GetComand(receiveMessage) == END) {
-				cout << "bye\n\n";
-				system("pause");
-				break;
-			}
-		}
-		//Çàêðûâàåì ñîåäèíåíèå
-		client.CloseConnection();
-		return 0;
-	}
-
-	break;
-
-	case 3:
-	{
-		return 0;
-	}
-	break;
-
-	default:
-		break;
-	}
+BOOL CALLBACK dlgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg)
+    {
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case IDOK:
+        case IDCANCEL:
+            EndDialog(hWnd, 0);
+            return TRUE;
+        }
+    }
+    return FALSE;
 }
+DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_SERVER), NULL, dlgProc);

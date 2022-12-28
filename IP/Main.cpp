f92e74c75137d71e5a9de314bf2226266f4701fa
Velmeri <danielmikhh2gmail.com>
2022-12-28 17:10:33 +0100

    1

--- IP/Main.cpp
+++ IP/Main.cpp
@@ -1,6 +1,10 @@
 ﻿//Main.cpp
 #include <iostream>
 #include "Socket.h"
+#include "resource1.h"
+#include <windows.H>
+#include <windowsX.h>
+#include <commctrl.h>
 
 using namespace std;
 
@@ -19,30 +23,103 @@ int GetComand(string comand) {
 	return InvalidСommand;
 }
 
-#pragma once
-
-#include "resource1.h"
-#include <windows.H>
-#include <windowsX.h>
-#include <commctrl.h>
-
 #pragma comment(lib,"comctl32")
 #pragma comment(lib, "winmm.lib")
 
 HINSTANCE hInst;
+Socket Server;
+Socket User;
 
-BOOL CALLBACK dlgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+HWND EDIT1;
+
+BOOL CALLBACK dlgProc1(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg)
     {
     case WM_COMMAND:
-        switch (LOWORD(wParam))
+        switch (msg)
         {
-        case IDOK:
-        case IDCANCEL:
+        case WM_INITDIALOG:
+        {
+            EDIT1 = GetWindow(hWnd, IDC_EDIT3);
+        }
+        break;
+        case WM_CLOSE:
+        {
             EndDialog(hWnd, 0);
-            return TRUE;
+        }
+        break;
+        case WM_COMMAND:
+        {
+        case IDC_SEND:
+            WCHAR buf[1024];
+            GetDlgItemText(hWnd, IDC_EDIT3, buf, 1024);
+            char I_hate_winAPI[1024];
+            for (int i = 0; i < 1024; i++)
+                I_hate_winAPI[i] = buf[i];
+            Server.SendData(I_hate_winAPI);
+        }
+        break;
+        case IDC_END:
+        {
+            Server.SendData("end");
+            Server.CloseConnection();
+        }
+        }
+
+    }
+    return FALSE;
+}
+
+BOOL CALLBACK dlgProc1(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg)
+    {
+    case WM_COMMAND:
+        switch (msg)
+        {
+        case WM_INITDIALOG:
+        {
+            EDIT1 = GetWindow(hWnd, IDC_EDIT3);
+        }
+        break;
+        case WM_CLOSE:
+        {
+            EndDialog(hWnd, 0);
+        }
+        break;
+        case WM_COMMAND:
+        {
+        case IDC_SEND:
+            WCHAR buf[1024];
+            GetDlgItemText(hWnd, IDC_EDIT3, buf, 1024);
+            char I_hate_winAPI[1024];
+            for (int i = 0; i < 1024; i++)
+                I_hate_winAPI[i] = buf[i];
+            User.SendData(I_hate_winAPI);
+        }
+        break;
+        case IDC_BREAK:
+            User.SendData("end");
         }
     }
     return FALSE;
 }
-DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_SERVER), NULL, dlgProc);
+
+int main() {
+    unsigned short res;
+    cin >> res;
+    switch (res)
+    {
+    case 1:
+    {
+        DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_SERVER), NULL, dlgProc1);
+    }
+    break;
+    case 2:
+    {
+        DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_SERVER), NULL, dlgProc2);
+    }
+    break;
+
+    }
+
+}
